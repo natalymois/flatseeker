64f2ad07405ec1cf4dcc21518ee57dd809dbab30
natalymois <mois-2011@mail.ru>
2018-03-20 10:15:47 +0200

    адаптивная версия
    адаптивная версия

--- js/index.js
+++ js/index.js
@@ -13,3 +13,22 @@ $(function() {
   $("#price-max").val($("#price-range").slider("values", 1));
   
 });
+
+/*----------------------------- выпадающее меню main-navigation------------------------------*/
+
+var mediaMenu = document.getElementById ('mediaMenu'); // mediaMenu == div.responsive-centerfix
+var menuA = mediaMenu.getElementsByTagName('a')[0];
+
+var mediaTopMenu = document.getElementsByClassName('hidden-top')[0];
+var menuTopA = mediaTopMenu.getElementsByTagName('a')[0];
+
+if (window.innerWidth <= 750) {
+	mediaMenu.onclick = function() {
+		this.classList.toggle('responsive');
+		menuA.classList.toggle('responsive');
+	}
+	mediaTopMenu.onclick = function() {
+		this.classList.toggle('responsive');
+		menuTopA.classList.toggle('responsive');
+	}
+}
